f836f96a163d24d2b0d456052abb350602ad8cbc
Davizukss <davivarelladev@gmail.com>
2024-09-02 11:48:48 -0300

    Criação de 5 tabelas no java

--- src/main/java/br/com/etechoracio/pw3_study/Entity/Monitor.java
+++ src/main/java/br/com/etechoracio/pw3_study/Entity/Monitor.java
@@ -1,4 +1,37 @@
 package br.com.etechoracio.pw3_study.Entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@Entity
+@Table(name = "TBL_MONITOR")
 public class Monitor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_MONITOR")
+    private Long id;
+
+    @Column(name = "TX_NOME")
+    private String tx_nome;
+
+    @Column(name = "TX_FOTO")
+    private String tx_foto;
+
+    @Column(name = "TX_WHATSAPP")
+    private String tx_whatsapp;
+
+    @Column(name = "TX_EMAIL")
+    private String tx_email;
+
+    @Column(name = "TX_CONTEUDO")
+    private String tx_conteudo;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_DISCIPLINA")
+    private Disciplina id_disciplina;
 }
--- src/main/java/br/com/etechoracio/pw3_study/Entity/MonitorDisponibilidade.java
+++ src/main/java/br/com/etechoracio/pw3_study/Entity/MonitorDisponibilidade.java
@@ -1,4 +1,23 @@
 package br.com.etechoracio.pw3_study.Entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TBL_REL_MONITOR_DISPONIBILIDADE")
 public class MonitorDisponibilidade {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "ID_MONITOR")
+    private Monitor id_monitor;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "ID_DISPONIBILIDADE")
+    private Disponibilidade id_disponibilidade;
+
 }
